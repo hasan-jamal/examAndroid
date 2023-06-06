b2b95a8a667dcff3f2816b5becf5a4c9356bf0ab
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2023-06-06 19:18:04 +0200

    first commit

---
+++ app/src/main/java/com/example/examhomework/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.examhomework;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize UI components
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginbtn);
+
+        // Set click listener for the login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                performLogin(username, password);
+            }
+        });
+
+        googleBtn = findViewById(R.id.google_btn);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            navigateToSecondActivity();
+        }
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+    private void performLogin(String username, String password) {
+        // Create OkHttpClient instance
+        OkHttpClient client = new OkHttpClient();
+
+        // Build the request body
+        RequestBody requestBody = new FormBody.Builder()
+                .add("Email", username)
+                .add("Password", password)
+                .build();
+
+        // Build the request
+        Request request = new Request.Builder()
+                .url("https://imanshahin-001-site1.gtempurl.com/api/User/login")
+                .post(requestBody)
+                .build();
+
+        // Make the API call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // Login successful
+                    String responseBody = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this,SecondActivity.class));
+                            // Handle successful login, e.g., navigate to the next activity
+                        }
+                    });
+                } else {
+                    // Login failed
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Login failed. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/examhomework/MyService.java
@@ -0,0 +1,41 @@
+package com.example.examhomework;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    MediaPlayer mediaplayer;
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaplayer = MediaPlayer.create(this,R.raw.alkother);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaplayer.isPlaying()){
+            mediaplayer.stop();
+            mediaplayer.release();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(!mediaplayer.isPlaying()){
+            mediaplayer.start();
+        }
+        return START_STICKY;
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
